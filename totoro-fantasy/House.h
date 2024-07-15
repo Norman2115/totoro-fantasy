baa7932b388694ba700ed71ac0f9040554da8d08
wy <liewwenyen@gmail.com>
2024-07-15 13:02:45 +0800

    house

--- totoro-fantasy/House.h
+++ totoro-fantasy/House.h
@@ -1,6 +1,5 @@
 #ifndef HOUSE_H
 #define HOUSE_H
-//help
 
 #include <GL/glut.h>
 
@@ -9,10 +8,7 @@ public:
     House() {}
 
     void display() {
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Set background color (light green)
-        glClearColor(0.678f, 1.0f, 0.184f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // House Body (dark blue) with half the previous width
